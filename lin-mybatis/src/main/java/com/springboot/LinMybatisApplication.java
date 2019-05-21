3f7421814cac00a4ca7f7d615dd7dad5dacb20f8
yanglin <xueshuiyy@126.com>
2019-05-21 11:25:27 +0800

    几个待整合模块的创建

---
+++ lin-mybatis/src/main/java/com/springboot/LinMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinMybatisApplication.class, args);
+	}
+
+}
---
+++ lin-swagger/src/main/java/com/springboot/LinSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinSwaggerApplication.class, args);
+	}
+
+}
